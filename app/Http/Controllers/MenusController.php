4a5befee99335d9cf91614b92cd56d7e8c73c8bd
Duncan Smith <duncanssmith@gmail.com>
2019-03-25 16:46:48 +0000

    Create user auth and menu model and controller, list view start

---
+++ app/Http/Controllers/MenusController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MenusController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('menus.index');
+    }
+
+}
