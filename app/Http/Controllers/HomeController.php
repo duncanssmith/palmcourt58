d2a6d21527f909a53b9585042d838fbe5a39cfbd
Duncan Smith <duncanssmith@gmail.com>
2019-04-02 14:25:16 +0100

    Put menu images on main page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,9 +24,24 @@ class HomeController extends Controller
      */
     public function welcome()
     {
+        $carouselImages = [
+            "../media/images/aMainFront-1.jpg",
+            "../media/images/aInteriorSpace-1.jpg",
+            "../media/images/aBreakfastBuffetRoom.jpg",
+            "../media/images/aBreakfastBuffetContinental.jpg",
+            "../media/images/aBreakfastBuffetBakery.jpg",
+            "../media/images/aMainFront-2.jpg",
+            "../media/images/aInteriorSpace-2.jpg",
+        ];
+//        {{-- <img src="/media/images/Home/aMainFront-3.jpg" width="400px"> --}}
+//        {{-- <img src="/media/images/Home/gCoffeeMachine.jpg" width="200px"> --}}
+//        {{-- <img src="/media/images/Home/aMainFront-4.jpg" width="400px"> --}}
+//        {{-- <img src="/media/images/Home/gCoffeePour.jpg" width="400px"> --}}
+//        {{-- <img src="/media/images/Home/gFacade.jpg" width="400px"> --}}
+
         $menus = DB::table('menus')->get();
 
-        return view('welcome', ['menus' => $menus]);
+        return view('welcome', ['menus' => $menus, 'carouselImages' => $carouselImages]);
     }
 
     /**
