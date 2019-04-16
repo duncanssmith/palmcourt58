23bfa8523a957b3662418d2f47c59dd29f4118d7
Duncan Smith <duncanssmith@gmail.com>
2019-04-16 15:29:00 +0100

    Nav bar improvements

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -8,21 +8,21 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto list-group">
+            <ul class="navbar-nav mr-auto ">
                 {{--<a class="navbar-brand" href="{{ url('/') }}">--}}
                 {{--{{ config('app.name', 'Palm Court Pavilion') }}--}}
                 {{--</a>--}}
-                <li class="list-group-item">
+                <li class="">
                     <a href="/" class="navbar-brand header-link">
                         Palm Court Pavilion
                         {{--<img id="nav-logo" src="/media/images/Home/logo-transparent.png" class="nav-logo" width="210" >--}}
                     </a>
                 </li>
                 {{--<li class="list-group-item"><a href="/">Welcome</a></li>--}}
-                <li class="list-group-item"><a href="/menu">Menu</a></li>
-                <li class="list-group-item"><a href="/views">Views</a></li>
-                <li class="list-group-item"><a href="/functions">Functions</a></li>
-                <li class="list-group-item"><a href="/about">About us</a></li>
+                <li class=""><a href="/menu" class="navbar">Menu</a></li>
+                <li class=""><a href="/views" class="navbar">Views</a></li>
+                <li class=""><a href="/functions" class="navbar">Functions</a></li>
+                <li class=""><a href="/about" class="navbar">About us</a></li>
 
 {{--                    @if(Auth::user())--}}
                     {{--<li class="list-group-item"><a href="/menus">Menus</a></li>--}}
@@ -45,7 +45,7 @@
                         {{--@endif--}}
                     @else
                         <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <a id="navbarDropdown" class="nav-link navbar dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
