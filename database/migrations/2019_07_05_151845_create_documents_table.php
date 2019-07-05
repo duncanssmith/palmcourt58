d1f88f68f654227365d8745af4391e0b10b5ccd4
Duncan Smith <duncanssmith@gmail.com>
2019-07-05 16:29:27 +0100

    Add documents functionality

---
+++ database/migrations/2019_07_05_151845_create_documents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('documents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('content');
+            $table->unsignedTinyInteger('active');
+            $table->unsignedSmallInteger('hierarchy');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('documents');
+    }
+}
