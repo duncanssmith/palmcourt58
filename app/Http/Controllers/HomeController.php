3b3dc484269ef6e17bbd67ceafe4476c0a04d5ff
Duncan Smith <duncanssmith@gmail.com>
2019-07-10 16:14:15 +0100

    Add auth to dashboard (home) page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,9 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-//        $this->middleware('auth');
+        // $this->middleware('auth');
+        // $this->middleware('auth')->except(['index']);
+         $this->middleware('auth')->only(['index']);
     }
 
     /**
