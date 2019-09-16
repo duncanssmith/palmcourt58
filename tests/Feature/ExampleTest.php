13321752fb7a7196c7f611aa684dc59b756dc6b9
Duncan Smith <duncanssmith@gmail.com>
2019-09-16 16:49:22 +0100

    Initial step creating feature tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -15,7 +15,43 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $response = $this->get('/');
+        $response->assertStatus(200);
+
+        $response = $this->get('/menus');
+        $response->assertStatus(200);
+
+        $response = $this->get('/views');
+        $response->assertStatus(200);
+
+        $response = $this->get('/functions');
+        $response->assertStatus(200);
+
+        $response = $this->get('/about');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicLoginTest()
+    {
+        $response = $this->get('/login');
 
         $response->assertStatus(200);
     }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicMenuTest()
+    {
+        $response = $this->get('/menu');
+
+        $response->assertStatus(200);
+    }
+
 }
